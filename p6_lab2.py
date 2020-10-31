d0f5108b87bd6b50b537cd0d5c5a5a6652719c9d
ahmadmuafiq <ahmadmuafiq15@outlook.com>
2020-10-31 16:42:45 +0700

    README.md

---
+++ p6_lab2.py
@@ -0,0 +1,11 @@
+a=int(input("masukkan nilai A : "))
+b=int(input("masukkan nilai B : "))
+print("variable A=",a)
+print("variable B=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil pejumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
